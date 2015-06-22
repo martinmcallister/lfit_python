4696b3b90d4d0345003860fb7ee5aaea71d7173a
Martin McAllister <mmcallister1@shef.ac.uk>
2015-06-22 15:13:47 +0100

    Getting mcmcfit_shared.py working with ulimb now an individual parameter

--- mcmcfit_shared.py
+++ mcmcfit_shared.py
@@ -143,7 +143,7 @@ class LCModel(MutableSequence):
         eclPars = [ \
             self.wdFlux[ecl].currVal, self.dFlux[ecl].currVal, self.sFlux[ecl].currVal, \
             self.rsFlux[ecl].currVal, self.q.currVal, self.dphi.currVal, self.rdisc[ecl].currVal, \
-            self.ulimb.currVal, self.rwd.currVal, self.scale[ecl].currVal, \
+            self.ulimb[ecl].currVal, self.rwd.currVal, self.scale[ecl].currVal, \
             self.az[ecl].currVal, self.fis[ecl].currVal, self.dexp[ecl].currVal, \
             self.phi0[ecl].currVal]
         if self.complex:
@@ -387,15 +387,13 @@ if __name__ == "__main__":
                 
         ind_npars = (npars-3)/neclipses
         a = 0 
-        b = 4     
+        b = 3     
         
         for iecl in range(neclipses):
             print "\nindividual params for eclipse %d:\n" % (iecl+1)
             p = 0+(iecl*ind_npars)
             a += (ind_npars*iecl)
             b += (a + ind_npars)
-            print a
-            print b
             if a <= npars:
 				for i in range(a,b):
 					if p == 4 or p == 5 or p == 8:
@@ -406,11 +404,11 @@ if __name__ == "__main__":
 							lolim,best,uplim = np.percentile(par,[16,50,84])
 							print "%s = %f +%f -%f" % (pars_unique[i],best,uplim-best,best-lolim)
 						else:
-							par = chain[:,p+4] 
+							par = chain[:,p+3] 
 							lolim,best,uplim = np.percentile(par,[16,50,84])
 							print "%s = %f +%f -%f" % (pars_unique[i],best,uplim-best,best-lolim)
 						p += 1	  
-				a = 4
+				a = 3
 				b = 0
             '''params.append(best)
             model[i] = best'''
